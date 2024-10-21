0026db2ce701553a2f7d3722928d16b0a827a656
z0012 <t.vafery61@gmail.com>
2024-10-22 00:10:47 +0330

    micro

---
+++ pulldowbn.ino
@@ -0,0 +1,20 @@
+int buttonpin=8;
+int ledPin=2;
+int buttonState;
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(ledPin,OUTPUT);
+  pinMode(buttonpin,INPUT);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  buttonState = digitalRead(buttonpin);
+  if (buttonState==LOW){
+    digitalWrite(ledPin,HIGH);}
+    else{
+      digitalWrite(ledPin,LOW);
+  }
+  
+
+}
